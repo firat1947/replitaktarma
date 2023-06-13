0229590e3145dd6d7616eacd8eddfe6468883752
Glitch (shadowrise) <none>
2023-06-13 10:43:52 +0000

    💐💼 Checkpoint ./komutlar/Yardım.js:24849461/30

--- komutlar/Yardım.js
+++ komutlar/Yardım.js
@@ -5,13 +5,13 @@ exports.run = async (client, message) => {
   let prefix = ayarlar.prefix;
 
   const embed = new Discord.MessageEmbed()
-    .setAuthor(`Windes`)
+    .setAuthor(`GladBot`)
     .setTitle(``)
     .setColor("#00ff00")
     .setThumbnail(client.user.avatarURL())
    .setImage('https://cdn.discordapp.com/attachments/1108819464524415097/1109034877774483466/standard_7.gif')
     .setDescription(
-      `🎧 Windes Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
+      `🎧 GladBot Botumuzu Eklemek İçin \`${prefix}davet\` yazabilirsiniz.`
     )
     .addField(
       `__Genel Komutlar__`,
